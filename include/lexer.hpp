2f15e80df3ad5ea701a1d40698deeec8065648c8
Armaan137 <a.khabra5@gmail.com>
2025-08-15 05:11:20 +0000

    finish most of the lexer

---
+++ include/lexer.hpp
@@ -0,0 +1,29 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <cstdio> 
+#include <cctype>
+#include <cstdlib>
+
+enum Token {
+  tok_eof = -1,
+
+  // commands.
+  tok_def = -2,
+  tok_extern = -3,
+
+  // primary.
+  tok_identifier = -4,
+  tok_number = -5,
+};
+
+// if the current token is an identifier, this stores it.
+extern std::string IdentifierStr;
+
+// if the current token is a numeric literal, this stores that.
+extern double NumVal;
+
+int gettok();
+
+#endif
---
+++ src/lexer.cpp
@@ -0,0 +1,64 @@
+#include "lexer.hpp"
+
+std::string IdentifierStr {};
+double NumVal {};
+
+// returns the next token from standard input.
+int gettok() {
+    static int lastChar {};
+
+    // skip whitespace.
+    while (std::isspace(lastChar)) {
+        lastChar = std::getchar();
+    }
+
+    if (std::isalpha(lastChar)) {
+        IdentifierStr = lastChar;
+
+        while (std::isalnum(lastChar = std::getchar())) {
+            IdentifierStr += lastChar;
+        }
+
+        if (IdentifierStr == "def") {
+            return tok_def;
+        } 
+
+        if (IdentifierStr == "extern") {
+            return tok_extern;
+        }
+
+        return tok_identifier;
+    }
+
+    if (std::isdigit(lastChar) || lastChar == '.') {
+        std::string numStr {};
+
+        do {
+            numStr += lastChar;
+            lastChar = std::getchar();
+        } while (std::isdigit(lastChar) || lastChar == '.');
+
+        NumVal = std::strtod(numStr.c_str(), 0);
+
+        return tok_number;
+    }
+
+    if (lastChar == '#') {
+        do {
+            lastChar = std::getchar();
+        } while (lastChar != EOF && lastChar != '\n' && lastChar != '\r');
+
+        if (lastChar != EOF) {
+            return gettok();
+        }
+    }
+
+    if (lastChar == EOF) {
+        return tok_eof;
+    }
+
+    int thisChar {lastChar};
+    lastChar = std::getchar();
+
+    return thisChar;
+}
