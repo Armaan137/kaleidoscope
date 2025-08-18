1a1480b17c7c780c303ee878779b7dc54bcfb839
Armaan137 <a.khabra5@gmail.com>
2025-08-18 02:58:39 +0000

    insert operators into main driver

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 
 int main() {
 
-    std::cout << "Runs.\n";
+    // insert the operators.
+    binaryPrecedence['<'] = 10; // lowest.
+    binaryPrecedence['+'] = 20;
+    binaryPrecedence['-'] = 30;
+    binaryPrecedence['*'] = 40; // highest.
 
     return 1;
 }
