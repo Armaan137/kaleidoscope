c32f938dbe54e42043f69a4de6ac960534070979
Armaan137 <a.khabra5@gmail.com>
2025-08-28 23:59:24 +0000

    rename global variables

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1,9 +1,9 @@
 #include "exprAST.hpp"
 #include "parser.hpp"
 
-static std::unique_ptr<llvm::LLVMContext> theContext;   // Object that has core LLVM data structures. Stored unique values.
+static std::unique_ptr<llvm::LLVMContext> context;   // Object that has core LLVM data structures. Stored unique values.
 static std::unique_ptr<llvm::IRBuilder<>> builder;      // Helper object that helps generate LLVM instructions. Instances of IRBuilder class template keeps track of the current place to insert and has methods to create new instructions. 
-static std::unique_ptr<llvm::Module> theModule;         // Construct that contains functions and global variables. Will own the memeory for all the IR generated.
+static std::unique_ptr<llvm::Module> module;         // Construct that contains functions and global variables. Will own the memeory for all the IR generated.
 static std::map<std::string, llvm::Value*> namedValues; // Keeps track of which values are defined in the current scope and what their LLVM representation is; symbol table.
 
 llvm::Value *logErrorV(const char *str) {
@@ -14,7 +14,7 @@ llvm::Value *logErrorV(const char *str) {
 llvm::Value *NumberExprAST::codegen() {
     // ConstantFP is a subclass of Constant which itself is a value. Represents a floating-point constant in LLVM IR.
     // APFloat is LLVM's internal class for representing floating-points of arbitrary precision.
-    return llvm::ConstantFP::get(*theContext, llvm::APFloat(val));
+    return llvm::ConstantFP::get(*context, llvm::APFloat(val));
 }
 
 llvm::Value *VariableExprAST::codegen() {
@@ -41,8 +41,9 @@ llvm::Value *BinaryExprAST::codegen() {
         case '<':
             l = builder->CreateFCmpULT(l, r, "cmptmp");
             // Convert the int bool value from CreateFCmpULT to a double.
-            return builder->CreateUIToFP(l, llvm::Type::getDoubleTy(*theContext), "booltmp");
+            return builder->CreateUIToFP(l, llvm::Type::getDoubleTy(*context), "booltmp");
         default:
             return logErrorV("Invalid binary operator.");
     }
 }
+
