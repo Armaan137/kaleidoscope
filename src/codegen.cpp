7ddb7860d00a3e7195b5ee022d2c786357640155
Armaan137 <a.khabra5@gmail.com>
2025-09-01 04:22:36 +0000

    remove statics

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1,12 +1,12 @@
 #include "exprAST.hpp"
 #include "parser.hpp"
 
-static std::unique_ptr<llvm::LLVMContext> context;      // Object that has core LLVM data structures. Stored unique values.
-static std::unique_ptr<llvm::IRBuilder<>> builder;      // Helper object that helps generate LLVM instructions. Instances of IRBuilder class template keeps track of the current place to insert and has methods to create new instructions. 
-static std::unique_ptr<llvm::Module> module;            // Construct that contains functions and global variables. Will own the memory for all the IR generated.
-static std::map<std::string, llvm::Value*> namedValues; // Keeps track of which values are defined in the current scope and what their LLVM representation is; symbol table.
+std::unique_ptr<llvm::LLVMContext> context;      // Object that has core LLVM data structures. Stored unique values.
+std::unique_ptr<llvm::IRBuilder<>> builder;      // Helper object that helps generate LLVM instructions. Instances of IRBuilder class template keeps track of the current place to insert and has methods to create new instructions. 
+std::unique_ptr<llvm::Module> module;            // Construct that contains functions and global variables. Will own the memory for all the IR generated.
+std::map<std::string, llvm::Value*> namedValues; // Keeps track of which values are defined in the current scope and what their LLVM representation is; symbol table.
 
-llvm::Value *logErrorV(const char *str) {
+static llvm::Value *logErrorV(const char *str) {
     logError(str);
     return nullptr;
 }
